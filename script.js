f19d3ea7c409bd9c903e2f89ea04d513b49b7ba3
nargesrzn <59249853+nargesrzn@users.noreply.github.com>
2020-05-01 00:20:07 +0430

    fixing innerHTML of mode on toggle

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
 function theme() {
   document.body.classList.toggle("light");
+   if (document.getElementById("mode").innerHTML == "Dark Mode") {
+    document.getElementById("mode").innerHTML = "Light Mode";
+  } else {
+    document.getElementById("mode").innerHTML = "Dark Mode";
+  }
 }
